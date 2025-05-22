743a4bb5893d8e8ccc7e99937fbceee0f59995a0
daneedev <dan@danee.dev>
2025-05-22 18:25:24 +0200

    copy from old repo

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express"
+import dotenv from "dotenv"
+import nunjucks from "nunjucks"
+import fs from "fs"
+dotenv.config()
+
+const app = express()
+
+
+nunjucks.configure('src/views', {
+    autoescape: true,
+    express: app,
+    watch: true
+});
+
+
+app.get("/", function (req: Request, res: Response) {
+    res.send("Hello world!")
+})
+
+app.get("/:file", function (req: Request, res: Response) {
+    if (!fs.readFileSync(`src/views/${req.params.file}`)) {
+        res.status(404).send("File not found")
+    } else {
+    res.render(`${req.params.file}`, {})
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on ${process.env.PORT}`)
+})
