af396ffd008e7482c186902721cb23a5ac3bf3ff
daneedev <dan@danee.dev>
2025-05-23 13:58:39 +0200

    business model

---
+++ src/models/Business.ts
@@ -0,0 +1,35 @@
+import { db } from "../handlers/db";
+import { DataTypes, Model } from "sequelize";
+
+class Business extends Model {
+    public id!: number;
+    public name!: string;
+    public ownerId!: string;
+    public createdAt!: Date;
+    public updatedAt!: Date;
+}
+
+Business.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ownerId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize: db,
+        tableName: "businesses",
+    }
+);
+
+
+export default Business;
