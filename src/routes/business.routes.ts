f48ed68bc09c9e1d8c128705069956ddf9a99ce1
GamaNoty <noga.ma.2023@skola.ssps.cz>
2025-05-26 14:24:51 +0200

    Updated route

--- src/routes/business.routes.ts
+++ src/routes/business.routes.ts
@@ -1,36 +1,37 @@
-import express, { Request, Response } from "express"
-import Business from "../models/Business"
-import User from "../models/User"
+import express, { Request, Response } from "express";
+import Business from "../models/Business";
+import User from "../models/User";
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/create", async function (req: Request, res: Response) {
-    const { name } = req.body
-    const user = req.user as User
-    Business.create({
-        name: name,
-        userId: user.id
-    })
-    req.flash("success", "Business created successfully")
-    res.redirect("/dash")
-})
+  const { name } = req.body;
+  const user = req.user as User;
+  Business.create({
+    name: name,
+    userId: user.id,
+  });
+  req.flash("success", "Business created successfully");
+  res.redirect("/dash");
+});
 
 router.post("/delete", function (req: Request, res: Response) {
-    const { id } = req.body
-    const user = req.user as User
-    Business.destroy({
-        where: {
-            id: id,
-            userId: user.id
-        }
-    }).then(() => {
-        req.flash("success", "Business deleted successfully")
-        res.redirect("/dash")
-    }).catch((err) => {
-        req.flash("error", "Error deleting business")
-        res.redirect("/dash")
+  const { id } = req.body;
+  const user = req.user as User;
+  Business.destroy({
+    where: {
+      id: id,
+      userId: user.id,
+    },
+  })
+    .then(() => {
+      req.flash("success", "Business deleted successfully");
+      res.redirect("/dash");
     })
-})
+    .catch((err) => {
+      req.flash("error", "Error deleting business");
+      res.redirect("/dash");
+    });
+});
 
-
-export default router
+export default router;
