f065e77e20f5e51e9c38d43b7b0cffe66aaece8a
GamaNoty <noga.ma.2023@skola.ssps.cz>
2025-05-26 14:23:43 +0200

    Updated index

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,72 @@
-import express, { Request, Response } from "express"
-import dotenv from "dotenv"
-import nunjucks from "nunjucks"
-import path from "path"
-import { connect, db } from "./handlers/db"
-import flash from "connect-flash"
-import session from "express-session"
-import bodyParser from "body-parser"
-import connectSessionSequelize from 'connect-session-sequelize';
-import loadPassport from "./handlers/passport"
-import passport from "passport"
+import bodyParser from "body-parser";
+import flash from "connect-flash";
+import connectSessionSequelize from "connect-session-sequelize";
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import session from "express-session";
+import nunjucks from "nunjucks";
+import passport from "passport";
+import path from "path";
+import { connect, db } from "./handlers/db";
+import loadPassport from "./handlers/passport";
 
-dotenv.config()
+dotenv.config();
 
 // ROUTES
-import authRoutes from "./routes/auth.routes"
-import dashRoutes from "./routes/dash.routes"
-import businessRoutes from "./routes/business.routes"
-import apiRoutes from "./routes/api.routes"
+import apiRoutes from "./routes/api.routes";
+import authRoutes from "./routes/auth.routes";
+import businessRoutes from "./routes/business.routes";
+import dashRoutes from "./routes/dash.routes";
 
-const app = express()
+const app = express();
 
-app.use(flash())
+app.use(flash());
 
 const SequelizeStore = connectSessionSequelize(session.Store);
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET ? process.env.SESSION_SECRET : "secret",
     resave: false,
     saveUninitialized: true,
-    cookie: { 
-        secure: process.env.NODE_ENV === "production",
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
-    store: new SequelizeStore({ db: db})
-}))
+    store: new SequelizeStore({ db: db }),
+  })
+);
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-nunjucks.configure('src/views', {
-    autoescape: true,
-    express: app,
-    watch: true
+nunjucks.configure("src/views", {
+  autoescape: true,
+  express: app,
+  watch: true,
 });
 
 // DB
-connect()
+connect();
 
 // PASSPORT
-app.use(passport.initialize())
-app.use(passport.session())
-loadPassport()
+app.use(passport.initialize());
+app.use(passport.session());
+loadPassport();
 
-app.use("/public", express.static('src/public'));
+app.use("/public", express.static("src/public"));
 
 app.get("/", function (req: Request, res: Response) {
-    res.send("Hello world!")
-})
+  res.send("Hello world!");
+});
 
-app.use("/auth", authRoutes)
-app.use("/dash", dashRoutes)
-app.use("/business", businessRoutes)
-app.use("/api", apiRoutes)
+app.use("/auth", authRoutes);
+app.use("/dash", dashRoutes);
+app.use("/business", businessRoutes);
+app.use("/api", apiRoutes);
 
-app.use("/", express.static('src/views'));
+app.use("/", express.static("src/views"));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on ${process.env.PORT}`)
-})
+  console.log(`Server is listening on ${process.env.PORT}`);
+});
