c15193dd5cd39786d116b815a428dbe14e2f9ed7
GamaNoty <noga.ma.2023@skola.ssps.cz>
2025-05-27 13:08:18 +0200

    fixed graph

--- src/public/js/charts.js
+++ src/public/js/charts.js
@@ -5,59 +5,71 @@ document.addEventListener("DOMContentLoaded", async function () {
     bar.style.width = completion * 20 + "%";
   });
 
-    const businessId = window.location.pathname.split("/").pop();
-    const businessData = await fetch(`/api/getBusinessData/${businessId}`)
-    .then(response => response.json())
-    .catch(error => console.error('Error fetching business data:', error));
-  
-    const satisfactionSum = businessData.ratings.reduce((sum, rating) => sum + rating.satisfaction, 0);
-    const satisfactionCount = businessData.ratings.length * 3;
-    new Chart(document.getElementById('satisfactionChart'), {
-        type: 'doughnut',
-        data: {
-        datasets: [{
-            data: [satisfactionSum, satisfactionCount - satisfactionSum ],
-            borderWidth: 0,
-            backgroundColor: ['#a259ff', '#2c2f4a'],
-            borderRadius: [10, {
-                innerEnd: 10,
-                outerEnd: 10
-            }]
-        }],
-        },
-        options: {
-            cutout: '80%',
-            plugins: {
-            tooltip: { enabled: false },
-            legend: { display: false },
+  const businessId = window.location.pathname.split("/").pop();
+  const businessData = await fetch(`/api/getBusinessData/${businessId}`)
+    .then((response) => response.json())
+    .catch((error) => console.error("Error fetching business data:", error));
+
+  const satisfactionSum = businessData.ratings.reduce(
+    (sum, rating) => sum + rating.satisfaction,
+    0
+  );
+  const satisfactionCount = businessData.ratings.length * 3;
+  new Chart(document.getElementById("satisfactionChart"), {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [satisfactionSum, satisfactionCount - satisfactionSum],
+          borderWidth: 0,
+          backgroundColor: ["#a259ff", "#2c2f4a"],
+          borderRadius: [
+            10,
+            {
+              innerEnd: 10,
+              outerEnd: 10,
             },
-            rotation: -90,
-            circumference: 180,
+          ],
         },
-    });
+      ],
+    },
+    options: {
+      cutout: "80%",
+      plugins: {
+        tooltip: { enabled: false },
+        legend: { display: false },
+      },
+      rotation: -90,
+      circumference: 180,
+    },
+  });
 
   const staff = document.getElementById("myChart2").getContext("2d");
 
-  const satisfactionRate = parseFloat("{{ stafSatisfactionRate }}") || 9.3;
-  const maxScore = 10;
-  const percentage = (satisfactionRate / maxScore) * 100;
-
+  const staffSum = businessData.ratings.reduce(
+    (sum, rating) => sum + rating.staff,
+    0
+  );
+  const staffCount = businessData.ratings.length * 4;
   const chart = new Chart(staff, {
     type: "doughnut",
     data: {
       datasets: [
         {
-          data: [percentage, 100 - percentage],
+          data: [staffSum, staffCount - staffSum],
           backgroundColor: [
             "rgb(16, 185, 129)", // Green for the progress
             "rgba(255, 255, 255, 0.1)", // Light transparent for the remaining
           ],
           borderWidth: 0,
           cutout: "75%",
-          borderRadius: [10, {
-                innerEnd: 10,
-                outerEnd: 10
-            }]
+          borderRadius: [
+            10,
+            {
+              innerEnd: 10,
+              outerEnd: 10,
+            },
+          ],
         },
       ],
     },
